58850b6b34e9666c1a0902cab0f31950cc6d1a11
Andrew Nurse <andrew@andrewnurse.net>
2013-05-19 19:06:13 -0600

    Create build-prep.sh

---
+++ build-prep.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository "deb http://debian.meebey.net/experimental/mono /"
+sudo apt-get update
+sudo apt-get install --allow-unauthenticated -q -y mono-devel=3.0.6+dfsg-1~exp1~pre1
+sudo apt-get install --allow-unauthenticated -q -y mono-gmcs=3.0.6+dfsg-1~exp1~pre1
